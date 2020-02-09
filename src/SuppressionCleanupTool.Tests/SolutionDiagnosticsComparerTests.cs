e790ab4cafe636eb3ea10659c32d44839529760c
jnm2 <me@jnm2.com>
2020-02-09 15:21:36 -0500

    Failing test that document options are passed to analyzers

---
+++ src/SuppressionCleanupTool.Tests/SolutionDiagnosticsComparerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using NUnit.Framework;
+using System;
+using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SuppressionCleanupTool.Tests
+{
+    public static class SolutionDiagnosticsComparerTests
+    {
+        [TestCase("IDE0031", TestSyntax.TriggeringIDE0031)]
+        [TestCase("IDE0044", TestSyntax.TriggeringIDE0044)]
+        [TestCase("IDE0062", TestSyntax.TriggeringIDE0062)]
+        public static async Task Document_options_are_provided_so_that_analyzers_return_diagnostics_as_normal(string diagnosticId, string syntaxTriggeringDiagnostic)
+        {
+            var syntaxTriggeringDiagnosticWithPragma = "#pragma warning disable " + diagnosticId + Environment.NewLine + syntaxTriggeringDiagnostic;
+
+            var newDiagnostics = await HasNewAnalyzerDiagnosticsAsync(
+                originalDocument: syntaxTriggeringDiagnosticWithPragma,
+                updatedDocument: syntaxTriggeringDiagnostic,
+                diagnosticId);
+
+            if (!newDiagnostics)
+            {
+                Assert.Fail($"Expected {diagnosticId} to be reported at least once in:" + Environment.NewLine + syntaxTriggeringDiagnostic);
+            }
+        }
+
+        private static async Task<bool> HasNewAnalyzerDiagnosticsAsync(string originalDocument, string updatedDocument, string diagnosticId)
+        {
+            using var workspace = new AdhocWorkspace();
+
+            workspace.AddSolution(SolutionInfo.Create(SolutionId.CreateNewId(), VersionStamp.Create()));
+
+            var document = workspace.AddProject("TestProject", LanguageNames.CSharp)
+                .AddDocument("TestDocument.cs", originalDocument);
+
+            var comparer = new SolutionDiagnosticsComparer(document.Project.Solution);
+
+            return await comparer.HasNewAnalyzerDiagnosticsAsync(document.WithText(
+                SourceText.From(updatedDocument)),
+                diagnosticIdFilter: ImmutableArray.Create(diagnosticId),
+                CancellationToken.None).ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/SuppressionCleanupTool.Tests/TestSyntax.cs
@@ -0,0 +1,27 @@
+﻿namespace SuppressionCleanupTool.Tests
+{
+    internal static class TestSyntax
+    {
+        public const string TriggeringIDE0031 = @"
+class C
+{
+    string M(object obj) => obj is null ? null : obj.ToString();
+}";
+
+        public const string TriggeringIDE0044 = @"
+class C
+{
+    int f = 4;
+}";
+
+        public const string TriggeringIDE0062 = @"
+class C
+{
+    void M()
+    {
+        LocalFunction();
+        void LocalFunction() { }
+    }
+}";
+    }
+}
