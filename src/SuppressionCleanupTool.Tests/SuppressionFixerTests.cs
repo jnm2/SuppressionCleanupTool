0408948be9f093552d47211bda1d8b0a2dc1f3fb
jnm2 <me@jnm2.com>
2020-02-09 19:28:04 -0500

    Failing test demonstrating https://github.com/dotnet/roslyn/issues/41526

--- src/SuppressionCleanupTool.Tests/SuppressionFixerTests.cs
+++ src/SuppressionCleanupTool.Tests/SuppressionFixerTests.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.CodeAnalysis.CSharp;
 using NUnit.Framework;
 using Shouldly;
+using SuppressionCleanupTool.Tests.Properties;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SuppressionCleanupTool.Tests
 {
@@ -27,6 +30,15 @@ class C
 }");
         }
 
+        [Test]
+        public static async Task Multiple_enumeration_of_SyntaxNode_Descendants_should_not_be_used_because_it_triggers_Roslyn_bug()
+        {
+            // This file repros https://github.com/dotnet/roslyn/issues/41526.
+            var documentText = Resources.AbstractAddImportFeatureService;
+
+            await FixAllInSingleDocumentWorkspaceAsync(documentText);
+        }
+
         private static string RemoveSingleSuppression(string compilationUnit, string syntaxToRemove)
         {
             var syntaxRoot = SyntaxFactory.ParseCompilationUnit(compilationUnit);
@@ -39,6 +51,18 @@ class C
                 .ShouldHaveSingleItem($"Expected exactly one removal in the compilation unit with the removed text '{syntaxToRemove}'.");
 
             return removal.NewRoot.ToFullString();
+        }
+
+        private static async Task<string> FixAllInSingleDocumentWorkspaceAsync(string documentText)
+        {
+            using var workspace = TestUtils.CreateSingleDocumentWorkspace(documentText, out var document);
+
+            var comparer = new SolutionDiagnosticsComparer(document.Project.Solution);
+
+            var fixedDocument = await SuppressionFixer.FixAllInDocumentAsync(document, comparer, afterRemoval: _ => { }, CancellationToken.None);
+
+            var fixedText = await fixedDocument.GetTextAsync();
+            return fixedText.ToString();
         }
     }
 }
